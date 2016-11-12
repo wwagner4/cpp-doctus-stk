0ea55bc61cf16c4f33e25e09273702e93649c08e
wwagner4 <wwagner4@gmail.com>
2016-11-12 22:33:01 +0100

    create nodes from extern

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,5 +1,179 @@
 #include "Node.hpp"
 
 namespace dstk {
+
+class NodeSine: public Node {
+
+	stk::SineWave sineWave;
+
+public:
+
+	NodeSine(stk::StkFloat frequency) {
+		printf("NodeSine\n");
+		sineWave.setFrequency(frequency);
+	}
+	~NodeSine() {
+		printf("~NodeSine\n");
+	}
+	stk::StkFloat tick() {
+		stk::StkFloat v = sineWave.tick();
+		return v;
+	}
+	void keyOn() {
+		printf("keyOn\n");
+		// Nothing to do
+	}
+	void keyOff() {
+		printf("keyOff\n");
+		// Nothing to do
+	}
+	void setValue(ValueType type, stk::StkFloat value) {
+		switch (type) {
+		case frequency:
+			sineWave.setFrequency(value);
+			break;
+		default:
+			printf("Could not set value for %d in NodeGain\n", type);
+		}
+	}
+
+};
+
+class NodeGain: public Node {
+
+	stk::StkFloat gain_;
+
+public:
+	NodeGain(stk::StkFloat gain) {
+		gain_ = gain;
+		printf("NodeSine\n");
+	}
+	NodeGain() {
+		gain_ = 1.0;
+		printf("NodeSine\n");
+	}
+	stk::StkFloat tick() {
+		return gain_;
+	}
+	void keyOn() {
+		printf("keyOn\n");
+		// Nothing to do
+	}
+	void keyOff() {
+		printf("keyOff\n");
+		// Nothing to do
+	}
+	void setValue(ValueType type, stk::StkFloat value) {
+		switch (type) {
+		case gain:
+			gain_ = value;
+			break;
+		default:
+			printf("Could not set value for %d in NodeGain\n", type);
+		}
+	}
+
+};
+
+class NodeSequence: public Node {
+
+	std::vector<Node*> nodes;
+
+public:
+	NodeSequence() {
+		printf("NodeSequence\n");
+	}
+	~NodeSequence() {
+		printf("~NodeSequence\n");
+		for (std::vector<Node*>::size_type i = 0; i != nodes.size(); i++) {
+			delete nodes[i];
+		}
+	}
+	stk::StkFloat tick() {
+		stk::StkFloat prod = 1.0;
+		for (std::vector<Node*>::size_type i = 0; i != nodes.size(); i++) {
+			prod *= nodes[i]->tick();
+		}
+		return prod;
+	}
+	void keyOn() {
+		printf("keyOn\n");
+		// Nothing to do
+	}
+	void keyOff() {
+		printf("keyOff\n");
+		// Nothing to do
+	}
+	void addNode(Node* node) {
+		printf("addNode\n");
+		nodes.push_back(node);
+	}
+	void setValue(ValueType type, stk::StkFloat value) {
+		printf("Could not set value for %d in NodeSequence\n", type);
+	}
+
+};
+
+class NodeSum: public Node {
+
+	std::vector<Node*> nodes;
+
+public:
+
+	NodeSum() {
+		printf("NodeSum\n");
+	}
+	~NodeSum() {
+		printf("~NodeSum\n");
+		for (std::vector<Node*>::size_type i = 0; i != nodes.size(); i++) {
+			delete nodes[i];
+		}
+	}
+	stk::StkFloat tick() {
+		stk::StkFloat sum = 0.0;
+		for (std::vector<Node*>::size_type i = 0; i != nodes.size(); i++) {
+			sum += nodes[i]->tick();
+		}
+		return sum;
+	}
+	void keyOn() {
+		printf("keyOn\n");
+		// Nothing to do
+	}
+	void keyOff() {
+		printf("keyOff\n");
+		// Nothing to do
+	}
+	void addNode(Node* node) {
+		printf("addNode\n");
+		nodes.push_back(node);
+	}
+	void setValue(ValueType type, stk::StkFloat value) {
+		printf("Could not set value for %d in NodeSum\n", type);
+	}
+
+};
+
+Node* NodeFactory::nodeSine(stk::StkFloat frequency) {
+	return new NodeSine(frequency);
+}
+Node* NodeFactory::nodeGain(stk::StkFloat gain) {
+	return new NodeGain(gain);
+}
+Node* NodeFactory::nodeSequence(std::vector<Node*> nodes) {
+	NodeSequence* re = new NodeSequence();
+	for (Node* node : nodes) {
+		re->addNode(node);
+	}
+	return re;
+}
+Node* NodeFactory::nodeSum(std::vector<Node*> nodes) {
+	NodeSum* re = new NodeSum();
+	for (Node* node : nodes) {
+		re->addNode(node);
+	}
+	return re;
+}
+
 }
 
--- src/Node.hpp
+++ src/Node.hpp
@@ -1,6 +1,12 @@
-#include "Stk.h"
+#include "SineWave.h"
+#include "RtWvOut.h"
 
 namespace dstk {
+
+enum ValueType {
+	frequency,
+	gain
+};
 
 class Node {
 
@@ -9,8 +15,19 @@ public:
 	virtual stk::StkFloat tick() = 0;
 	virtual void keyOn() = 0;
 	virtual void keyOff() = 0;
-	virtual ~Node() {
-	};
+	virtual void setValue(ValueType type, stk::StkFloat value) = 0;
+	virtual ~Node() {};
 
 };
+
+class NodeFactory {
+
+public:
+	Node* nodeSine(stk::StkFloat frequency);
+	Node* nodeGain(stk::StkFloat gain);
+	Node* nodeSequence(std::vector<Node*> nodes);
+	Node* nodeSum(std::vector<Node*> nodes);
+
+};
+
 }
