9baae471deb2b8b38b71b256547b74edd40f95d6
wwagner4 <wwagner4@gmail.com>
2016-12-07 22:11:02 +0100

    added J Node, Gain, Sine

---
+++ src/JStkNodeGain.java
@@ -0,0 +1,12 @@
+public class JStkNodeGain implements JStkNode {
+  
+  private int id;
+  
+  public JStkNodeGain(int id) {
+    this.id = id;
+  }
+  
+  public int getId() {
+    return id;
+  }
+}
---
+++ src/JStkNodeSine.java
@@ -0,0 +1,12 @@
+public class JStkNodeSine implements JStkNode {
+  
+  private int id;
+  
+  public JStkNodeSine(int id) {
+    this.id = id;
+  }
+  
+  public int getId() {
+    return id;
+  }
+}
