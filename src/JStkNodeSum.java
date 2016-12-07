067e1c75cd58415f9a3116b53410207c4944273d
wwagner4 <wwagner4@gmail.com>
2016-12-07 22:49:05 +0100

    added sum node

---
+++ src/JStkNodeSum.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class JStkNodeSum implements JStkNode, JStkContainer {
+  
+  private int id;
+  private List<JStkNode> children;
+  
+  public JStkNodeSum(int id, List<JStkNode> children) {
+    this.id = id;
+    this.children = children;
+  }
+  
+  public int getId() {
+    return id;
+  } 
+  
+  public List<JStkNode> getChildren() {
+    return this.children;
+  }
+  
+}
