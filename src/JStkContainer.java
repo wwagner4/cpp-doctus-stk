dc57e069847233ccad5ad9df399d9104ef39c1c2
wwagner4 <wwagner4@gmail.com>
2016-12-07 22:27:17 +0100

    added sequence node

---
+++ src/JStkContainer.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public interface JStkContainer {
+  
+  List<JStkNode> getChildren();
+  
+}
---
+++ src/JStkNodeSequence.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class JStkNodeSequence implements JStkNode, JStkContainer {
+  
+  private int id;
+  private List<JStkNode> children;
+  
+  public JStkNodeSequence(int id, List<JStkNode> children) {
+    this.id = id;
+    this.children = children;
+  }
+  
+  public int getId() {
+    return id;
+  } 
+  
+  public List<JStkNode> getChildren() {
+    return this.children;
+  }
+  
+}
