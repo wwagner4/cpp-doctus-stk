a73efeac1b3eb9b0924dd366441d0df80bda055b
wwagner4 <wwagner4@gmail.com>
2016-12-08 17:58:35 +0100

    handling of graph works fine

--- src/JStk.cpp
+++ src/JStk.cpp
@@ -3,6 +3,7 @@
 
 #include <cstdio>
 #include <list>
+#include <map>
 #include <random>
 
 #include "Node.hpp"
@@ -12,9 +13,42 @@
 using namespace stk;
 using namespace jstk;
 
+/**
+ * Holds all resources necessary for executing a graph
+ */
+class Graph {
+  private:
+    Node* node;
+    RtWvOut* dac;
+
+  public:
+
+    int id;
+    bool running = true;
+
+    Graph(int id, Node* node) {
+      this->id = id;
+      this->node = node;
+      this->dac = new RtWvOut(2);
+    }
+
+    ~Graph() {
+      running = false;
+      delete this->node;
+      delete this->dac;
+    }
+
+    void tick() {
+      stk::StkFloat v = node->tick();
+      dac->tick(v);
+    }
+};
+
 NodeFactory f;
 
 int id = 0;
+
+std::map<int, Graph*> graphs;
 
 Node* createSine(StkFloat frequency, StkFloat gain) {
   Node* sine = f.nodeSine(id++, frequency);
@@ -35,23 +69,52 @@ Node* createNode() {
   return createSine(500, 1.0);
 }
 
+/**
+ * Utillity function for accessing 'map'
+ */
+void addToGraphs(int id, Graph* graph) {
+  std::pair<std::map<int, Graph*>::iterator,bool> ret;
+  ret = graphs.insert (std::pair<int, Graph*>(id, graph));
+  if (ret.second==false) {
+    delete graph;
+    char* msg;
+    sprintf(msg, "ERROR: A graph with id %d already exists", id);
+    throw std::domain_error(msg);
+  } else {
+    printf("Added graph with id %d\n", id);
+  }
+}
+
+/**
+ * Utillity function for accessing 'map'
+ */
+Graph* removeFromGraphs(int id) {
+  std::map<int, Graph*>::iterator it;
+  it = graphs.find(id);
+  if (it != graphs.end()) {
+    graphs.erase (it);
+    return it->second;
+  } else {
+    printf("C removeFromGraphs E\n");
+    char* msg;
+    sprintf(msg, "No graph with id %d existed", id);
+    throw std::domain_error(msg);
+  }
+}
+
 JNIEXPORT void JNICALL Java_JStk_addGraph
-    (JNIEnv *, jobject, jint graphId, jobject graph) {
+    (JNIEnv *, jobject, jint graphId, jobject jgraph) {
   try {
-    printf("C addGraph %d %p\n", graphId, graph);
+    printf("C addGraph %d %p\n", graphId, jgraph);
     Stk::showWarnings(true);
 
-    jstk::Node* node = createNode();
-    node->keyOn();
-    RtWvOut *dac = 0;
-    dac = new RtWvOut(2);
-    for (int f = 0; f < 100000; f++) {
-      stk::StkFloat v = node->tick();
-      dac->tick(v);
+    Node* node = createNode();
+    Graph* graph = new Graph(graphId, node);
+    addToGraphs(graphId, graph);
+
+    while(graph->running) {
+      graph->tick();
     }
-    node->keyOff();
-    delete node;
-    delete dac;
   } catch (...) {
     printf("ERROR: An unknown exception occurred in 'Java_JStk_addGraph\n'");
   }
@@ -59,6 +122,15 @@ JNIEXPORT void JNICALL Java_JStk_addGraph
 
 JNIEXPORT void JNICALL Java_JStk_removeGraph
   (JNIEnv *, jobject, jint graphId) {
-  printf("C removeGraph %d\n", graphId);
+  try {
+    printf("C removeGraph %d\n", graphId);
+    Graph* graph = removeFromGraphs(graphId);
+    graph->running = false;
+    delete graph;
+  } catch (...) {
+    printf("ERROR: An unknown exception occurred in 'Java_JStk_addGraph\n'");
+  }
+
+
 }
 
