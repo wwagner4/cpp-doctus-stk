6f0f2dd13a17ec2e855434ec0a17f7cfab6ee848
wwagner4 <wwagner4@gmail.com>
2016-12-10 08:51:38 +0100

    some renaming

--- src/NodeConverter.cpp
+++ src/NodeConverter.cpp
@@ -6,60 +6,59 @@ using namespace stk;
 
 namespace jstk {
 
-  Node* NodeConverter::createNodeSine(JNIEnv* env ,jobject jnode, jclass clazz, NodeFactory* factory) {
-    jmethodID idMid = env->GetMethodID(clazz, "getId", "()I");
-    jint nodeId = env->CallIntMethod(jnode, idMid);
+  Node* NodeConverter::createNodeSine(JNIEnv* env ,jobject nodeObj, jclass nodeClass, NodeFactory* factory) {
+    jmethodID idMid = env->GetMethodID(nodeClass, "getId", "()I");
+    jint nodeId = env->CallIntMethod(nodeObj, idMid);
 
-    jmethodID freqMid = env->GetMethodID(clazz, "getFrequency", "()D");
-    jdouble frequency = env->CallDoubleMethod(jnode, freqMid);
+    jmethodID freqMid = env->GetMethodID(nodeClass, "getFrequency", "()D");
+    jdouble frequency = env->CallDoubleMethod(nodeObj, freqMid);
 
     return factory->nodeSine(nodeId, frequency);
   }
 
-  Node* NodeConverter::createNodeGain(JNIEnv* env ,jobject jnode, jclass clazz, NodeFactory* factory) {
-    jmethodID idMid = env->GetMethodID(clazz, "getId", "()I");
-    jint nodeId = env->CallIntMethod(jnode, idMid);
+  Node* NodeConverter::createNodeGain(JNIEnv* env ,jobject nodeObj, jclass nodeClass, NodeFactory* factory) {
+    jmethodID idMid = env->GetMethodID(nodeClass, "getId", "()I");
+    jint nodeId = env->CallIntMethod(nodeObj, idMid);
 
-    jmethodID gainMid = env->GetMethodID(clazz, "getGain", "()D");
-    jdouble gain = env->CallDoubleMethod(jnode, gainMid);
+    jmethodID gainMid = env->GetMethodID(nodeClass, "getGain", "()D");
+    jdouble gain = env->CallDoubleMethod(nodeObj, gainMid);
 
     return factory->nodeGain(nodeId, gain);
   }
 
-  Node* NodeConverter::createNodeSequence(JNIEnv* env ,jobject jnode, jclass clazz, NodeFactory* factory) {
-    jmethodID idMid = env->GetMethodID(clazz, "getId", "()I");
-    jint nodeId = env->CallIntMethod(jnode, idMid);
+  Node* NodeConverter::createNodeSequence(JNIEnv* env ,jobject nodeObj, jclass nodeClass, NodeFactory* factory) {
+    jmethodID idMid = env->GetMethodID(nodeClass, "getId", "()I");
+    jint nodeId = env->CallIntMethod(nodeObj, idMid);
 
-    jmethodID getChildrenMid = env->GetMethodID(clazz, "getChildren", "()Ljava/util/List;");
-    jobject lObj = env->CallObjectMethod(jnode, getChildrenMid);
-    jclass clazz1 = env->GetObjectClass(lObj);
+    jmethodID getChildrenMid = env->GetMethodID(nodeClass, "getChildren", "()Ljava/util/List;");
+    jobject lObj = env->CallObjectMethod(nodeObj, getChildrenMid);
+    jclass lClass = env->GetObjectClass(lObj);
 
-    jmethodID sizeMid = env->GetMethodID(clazz1, "size", "()I");
-    jmethodID getMid = env->GetMethodID(clazz1, "get", "(I)Ljava/lang/Object;");
+    jmethodID sizeMid = env->GetMethodID(lClass, "size", "()I");
+    jmethodID getMid = env->GetMethodID(lClass, "get", "(I)Ljava/lang/Object;");
 
     jint size = env->CallIntMethod(lObj, sizeMid);
     std::list<Node*> v;
     for (int i=0; i<size; i++) {
-      jobject dObj = env->CallObjectMethod(lObj, getMid, i);
-      Node* n = this->createNode(env, dObj, factory);
+      jobject eObj = env->CallObjectMethod(lObj, getMid, i);
+      Node* n = this->createNode(env, eObj, factory);
       v.push_back(n);
     }  
     return factory->nodeSequence(nodeId, v);
 
   }
 
-
-  Node* NodeConverter::createNode(JNIEnv* env ,jobject nodeObj, NodeFactory* nodeFactory) {
-    jclass clazz = env->GetObjectClass(nodeObj);
-    jmethodID mid = env->GetMethodID(clazz, "getNodeClassOrdinal", "()I");
+  Node* NodeConverter::createNode(JNIEnv* env ,jobject nodeObj, NodeFactory* factory) {
+    jclass nodeClass = env->GetObjectClass(nodeObj);
+    jmethodID mid = env->GetMethodID(nodeClass, "getNodeClassOrdinal", "()I");
     jint nodeClassOrd = env->CallIntMethod(nodeObj, mid);
     switch (nodeClassOrd) {
       case NODE_CLASS_SINE:
-        return this->createNodeSine(env, nodeObj, clazz, nodeFactory);
+        return this->createNodeSine(env, nodeObj, nodeClass, factory);
       case NODE_CLASS_GAIN:
-        return this->createNodeGain(env, nodeObj, clazz, nodeFactory);
+        return this->createNodeGain(env, nodeObj, nodeClass, factory);
       case NODE_CLASS_SEQUENCE:
-        return this->createNodeSequence(env, nodeObj, clazz, nodeFactory);
+        return this->createNodeSequence(env, nodeObj, nodeClass, factory);
       case NODE_CLASS_SUM:
         throw std::domain_error("NodeConverter::createNode - NODE_CLASS_SUM - NOT YET IMPLEMENTED ");
       default:
