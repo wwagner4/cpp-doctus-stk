1ac5e4ed390c8b368762bc5051ec5133b87804fb
wwagner4 <wwagner4@gmail.com>
2016-12-16 08:03:03 +0100

    added scheduler

--- src/slope/SlopeTryout.java
+++ src/slope/SlopeTryout.java
@@ -1,9 +1,29 @@
 package slope;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class SlopeTryout {
 
 	public static void main(String[] args) {
 
+		// linearSlopeValues();
+		ScheduledExecutorService es = Executors.newScheduledThreadPool(20);
+		es.scheduleAtFixedRate(() -> System.out.printf("!!\n"), 0, 500, TimeUnit.MILLISECONDS);
+		pause(2000);
+		es.shutdown();
+	}
+
+	private static void pause(int milis) {
+		try {
+			Thread.sleep(milis);
+		} catch (InterruptedException e) {
+			// Nothing to do here
+		}
+	}
+
+	static void linearSlopeValues() {
 		{
 			System.out.printf("-- Ascending slope in 20 steps\n");
 			Slope s = new Slope();
@@ -46,8 +66,7 @@ public class SlopeTryout {
 			System.out.printf("-- Ascending slope in less than one step\n");
 			double fl = 1.0 / ISlope.FRAME_RATE;
 			System.out.printf("-- FrameLength %5.2f seconds\n", fl);
-			
-			
+
 			Slope s = new Slope();
 			ISlope ls = s.linear(fl * 0.9, 10, 20);
 			System.out.printf("BS %5.2f\n", ls.nextVal());
@@ -65,8 +84,7 @@ public class SlopeTryout {
 			System.out.printf("-- Descending slope in less than one step\n");
 			double fl = 1.0 / ISlope.FRAME_RATE;
 			System.out.printf("-- FrameLength %5.2f seconds\n", fl);
-			
-			
+
 			Slope s = new Slope();
 			ISlope ls = s.linear(fl * 0.1, 100, -20);
 			System.out.printf("BS %5.2f\n", ls.nextVal());
