e82abb6efc8b5f958bc99d0930a713b28c42672e
wwagner4 <wwagner4@gmail.com>
2016-12-16 22:03:43 +0100

    parameter executor works fine

--- src/slope/SlopeTryout.java
+++ src/slope/SlopeTryout.java
@@ -1,20 +1,27 @@
 package slope;
 
+import java.util.Optional;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 public class SlopeTryout {
 
 	public static void main(String[] args) {
-
-		// linearSlopeValues();
-		ScheduledExecutorService es = Executors.newScheduledThreadPool(20);
-		long periode = Double.valueOf(1000000.0 / ISlope.FRAME_RATE).longValue();
-		System.out.printf("periode: %d micros\n", periode);
-		es.scheduleAtFixedRate(() -> System.out.printf("!!\n"), 0, periode, TimeUnit.MICROSECONDS);
-		pause(100);
+		ScheduledExecutorService es  = Executors.newScheduledThreadPool(20);
+		
+		TParam p = new TParam();
+		ParamExec e = new ParamExec(p);
+		
+		ISlope s = new Slope().linear(0.1, 0, 100);
+		e.runSlope(s, es);
+		pause(50);
+		s.start();
+		pause(150);
+		
 		es.shutdown();
+		
 	}
 
 	private static void pause(int milis) {
@@ -100,6 +107,58 @@ public class SlopeTryout {
 			System.out.printf("S #3 %5.2f\n", ls.nextVal());
 			System.out.printf("S #4 %5.2f\n", ls.nextVal());
 		}
+	}
+
+}
+
+class ParamExec {
+
+	long periode = Double.valueOf(1000000.0 / ISlope.FRAME_RATE).longValue();
+
+	private Param param;
+
+	Optional<ScheduledFuture<?>> process = Optional.empty();
+
+	public ParamExec(Param param) {
+		this.param = param;
+	}
+
+	public void runSlope(ISlope slope, ScheduledExecutorService es) {
+
+		Runnable run = new Runnable() {
+
+			@Override
+			public void run() {
+				param.setValue(slope.nextVal());
+			}
+
+		};
+		process = Optional.of(es.scheduleAtFixedRate(run, 0, periode, TimeUnit.MICROSECONDS));
+	}
+
+}
+
+interface Param {
+
+	void setValue(double v);
+
+	double getValue();
+}
+
+class TParam implements Param {
+
+	double v = 0.0;
+
+	@Override
+	public void setValue(double v) {
+		System.out.printf("-> TParam setValue %.3f\n", v);
+		this.v = v;
+	}
+
+	@Override
+	public double getValue() {
+		System.out.printf("<- TParam getValue %.3f\n", v);
+		return v;
 	}
 
 }
